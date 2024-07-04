0332e6706941cc17dd88b74ad5a137b88f84e3d3
João Gabriel da Conceição Santos <gs495770@gmail.com>
2024-07-04 12:54:22 -0300

    feat: create configuration

---
+++ src/main/java/com/api/magalu/config/swagger/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.api.magalu.config.swagger;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI magaluOpenAPI(){
+        return new OpenAPI()
+                .info(new Info().title("Magalu backend challenge REST API")
+                        .description("REST API for Magalu backend challenge")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("GitHub Repository")
+                        .url("https://github.com/gabrieudev/magalu/"));
+    }
+}
