67ac7163b1af91219c4edd91da2ebdc32fc0d016
João Gabriel da Conceição Santos <gs495770@gmail.com>
2024-07-04 13:01:29 -0300

    feat: create repository

---
+++ src/main/java/com/api/magalu/repository/CommunicationRepository.java
@@ -0,0 +1,9 @@
+package com.api.magalu.repository;
+
+import com.api.magalu.model.Communication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommunicationRepository extends JpaRepository<Communication, Long> {
+}
