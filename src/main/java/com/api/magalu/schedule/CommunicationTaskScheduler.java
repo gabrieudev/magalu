6b264f2e34a7e73f076721468dc0f7334dd4a1af
João Gabriel da Conceição Santos <gs495770@gmail.com>
2024-07-04 16:00:11 -0300

    fix: enable scheduling

--- src/main/java/com/api/magalu/schedule/CommunicationTaskScheduler.java
+++ src/main/java/com/api/magalu/schedule/CommunicationTaskScheduler.java
@@ -11,7 +11,7 @@ import java.util.concurrent.TimeUnit;
 
 @Component
 @Slf4j
-public class TaskScheduler {
+public class CommunicationTaskScheduler {
 
     @Autowired
     private CommunicationService communicationService;
