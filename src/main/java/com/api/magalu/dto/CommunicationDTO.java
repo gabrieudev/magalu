50a1e0a844a9bbe43a03c40a2f5409133680f3d8
João Gabriel da Conceição Santos <gs495770@gmail.com>
2024-07-04 12:48:27 -0300

    feat: create dto

---
+++ src/main/java/com/api/magalu/dto/CommunicationDTO.java
@@ -0,0 +1,35 @@
+package com.api.magalu.dto;
+
+import com.api.magalu.model.Channel;
+import com.api.magalu.model.Status;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+public class CommunicationDTO {
+
+    private Long id;
+
+    @NotBlank
+    private String receiver;
+
+    @NotBlank
+    private String message;
+
+    @NotNull
+    @Future
+    private LocalDateTime dateTime;
+
+    @NotNull
+    private Channel channel;
+
+    @NotNull
+    private Status status;
+
+}
